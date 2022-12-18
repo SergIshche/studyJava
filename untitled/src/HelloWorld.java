9bc4b4166ff04048de35bb7ff1a7b553b18c149e
Serg <ishchuk.serhii8@gmail.com>
2022-12-18 11:14:40 +0200

    Start the project

---
+++ untitled/src/HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+    public static void main(String [] args){
+        System.out.println("Hello world");
+        int myInt;
+        short myShort = 22654;
+        long myLong = 4555555;
+        double myDouble = 235.45;
+        float myFloat = 15468.4f;
+        char c = 'a';
+        boolean b = true;
+        byte meByte = 100; //-128-128
+        myInt = 5577;
+        System.out.println(myInt);
+        System.out.println("My number " + myDouble);
+    }
+}
